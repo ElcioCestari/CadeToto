130bacd18d7115d536d5d43034b9fbc53cc9ae2d
Elcio Cestari Taira <elciotaira@gmail.com>
2019-05-29 19:10:34 -0400

    Criado o método que faz a leitura dos "animais" no firebase e exibe na petlistactivity.

--- app/src/main/java/com/elcio/myapplication/PetListActivity.java
+++ app/src/main/java/com/elcio/myapplication/PetListActivity.java
@@ -2,21 +2,29 @@ package com.elcio.myapplication;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.elcio.myapplication.model.Pet;
 import com.elcio.myapplication.model.User;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class PetListActivity extends AppCompatActivity {
 
     private ArrayList<Pet> petList;
+    private String[] stringPetList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,39 +32,65 @@ public class PetListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pet_list);
 
         petList = new ArrayList<>();
-        petList.add(new Pet("1", "toto", "1")); //apenas para teste
-        petList.add(new Pet("2", "bato", "2")); //apenas para teste
 
-        /*converte stringPetList ArrayList<Pet> em String[]*/
-        String[] stringPetList = new String[petList.size()];
-        for(int i = 0; i < petList.size(); i++){
-            stringPetList[i] = petList.get(i).toString();
-        }
-
-        /*Cria uma simples listview para ser exibida*/
-        ArrayAdapter<String> adapter =
-                new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, stringPetList);
-
-        ListView listView = findViewById(R.id.listView_pet_list);
-        listView.setAdapter(adapter);
+        getPetsFromFirebase();
 
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         /*
-        * Quando o usuário deseja cadastrar um novo pet ele deve clicar nessa view.
-        * Ela ira chamar uma activity que faz esse processamento
-        */
+         * Quando o usuário deseja cadastrar um novo pet ele deve clicar nessa view.
+         * Ela ira chamar uma activity que faz esse processamento
+         */
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 Intent intent = new Intent(getApplication(), CadastrarPetActivity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    /**
+     * esse método é responsavel por acessar o firebase,
+     * o nó "animal",
+     * percorrer todas as instancias e adicioná-los na variavel petlist.
+     */
+    private void getPetsFromFirebase() {
+        FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+        DatabaseReference reference = firebaseDatabase.getReference("animal");
+
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Pet pet;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    pet = snapshot.getValue(Pet.class);
+                    petList.add(pet);
+                    Log.d("LISTA DE ANIMAIS", "PETS: " + petList.get(0));
+                }
+
+                /*converte stringPetList ArrayList<Pet> em String[]*/
+                stringPetList = new String[petList.size()];
+                for (int i = 0; i < petList.size(); i++) {
+                    stringPetList[i] = petList.get(i).toString();
+                    Log.d("PET ATUAIS", "\n\n pet atuais: " + petList.get(i));
+
+                }
+
+                /*Cria uma simples listview para ser exibida*/
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, stringPetList);
+                ListView listView = findViewById(R.id.listView_pet_list);
+                listView.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
 }
